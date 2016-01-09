83b949f754e0c47a9cd7a75921d5fd8071ae7fe3
Mark Carter <mark4carter@gmail.com>
2016-01-09 18:12:12 -0500

    added regex for email n zip

--- src/pdfparse/core.clj
+++ src/pdfparse/core.clj
@@ -4,13 +4,25 @@
 						[java.net URL])
   (:gen-class))
 
+(def emailReg "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
+(def zipReg "[0-9]{5}(?:-[0-9]{4})?")
+
 (defn text-of-pdf
   [url]
   (with-open [pd (PDDocument/load (URL. url))]
     (let [stripper (PDFTextStripper.)]
     (.getText stripper pd))))
 
+(defn separate-cat
+	[results]
+	{:email (remove nil? (for [result results] (re-seq (re-pattern (str emailReg)) result)))
+		:zip (remove nil? (for [result results] (re-seq (re-pattern (str zipReg)) result)))})
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (spit (first args) (text-of-pdf (first (rest args)))))
+  (spit (first args)
+  	(separate-cat
+	  	(re-seq 
+	  		(re-pattern (str emailReg "|" zipReg)) 
+	  			(text-of-pdf (first (rest args)))))))
